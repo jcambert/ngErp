c39c8ac3bb11099f95baf230a110bd69f422ea03
jcambert <jc.ambert@free.fr>
2016-07-18 22:39:08 +0200

    First Commit

--- assets/js/public/signup/SignupModule.js
+++ assets/js/public/signup/SignupModule.js
@@ -1,2 +1,8 @@
-angular.module('ngErpSignup',['ngMaterial'])
+angular
+.module('ngErpSignup',['ngMaterial', 'ngMessages'])
+.config(function($mdIconProvider) {
+    $mdIconProvider
+       .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
+       .defaultIconSet('img/icons/sets/core-icons.svg', 24);
+   })
 ;
